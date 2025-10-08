73471f829b23e7a8eb470066f86959cf3eef1a87
Kiellen <kiellenkarolinne123@icloud.com>
2025-10-08 17:45:25 -0300

    upando aula 02

---
+++ aula02/App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import CartaoApresentacao from './src/components/CartaoApresentacao'
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems:'center'}}>
+      <CartaoApresentacao/>
+    </View>
+  )
+}
+
+export default App
