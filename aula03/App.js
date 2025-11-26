f8708e393f30c23bf1570095a7aa823448a73db7
Kiellen <kiellenkarolinne123@icloud.com>
2025-11-26 19:22:47 -0300

    Adiciona firebaseConfig e atualiza HomeScreen

--- aula03/App.js
+++ aula03/App.js
@@ -1,11 +1,22 @@
 import { View, Text } from 'react-native'
-import LoginScreen from './src/screens/LoginScreens'
+import LoginScreen from './src/screens/LoginScreen'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import HomeScreen from './src/screens/HomeScreen'
+import DetailsScreen from './src/screens/DetailsScreen'
+
+const Stack = createNativeStackNavigator()
 
 const App = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <LoginScreen />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerTitleAlign: 'center' }}>
+        <Stack.Screen name="Login" component={LoginScreen} options={{ title: "Tela de Login"  }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Cursos"  }} />
+        <Stack.Screen name="Details" component={DetailsScreen} options={{ title: "Detalhes do Curso"  }} />
+        {/* Cada Screen recebe uma navigation como prop */}
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
